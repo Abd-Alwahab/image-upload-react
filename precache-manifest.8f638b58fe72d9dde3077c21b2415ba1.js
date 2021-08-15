4bcc20c35a35a2961c1f54f6eeea99b809355101
AbdAlwahab <abdalwahabb7@gmail.com>
2021-08-15 10:49:43 +0300

    Updates

---
+++ precache-manifest.8f638b58fe72d9dde3077c21b2415ba1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11734bc7d9599705964b30d36d14950e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fc202aa9e99e979c5f75",
+    "url": "./static/css/main.8acf09ec.chunk.css"
+  },
+  {
+    "revision": "cfab97597d5b11b06b4c",
+    "url": "./static/js/2.0f9bd9d7.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.0f9bd9d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc202aa9e99e979c5f75",
+    "url": "./static/js/main.03982330.chunk.js"
+  },
+  {
+    "revision": "c3fcd17526cad3d77bf0",
+    "url": "./static/js/runtime-main.68905efa.js"
+  }
+]);
